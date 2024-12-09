7d3d4ac53b756290c880216ecf186f71a4f53121
wismannur <wismannura@gmail.com>
2024-12-09 18:01:19 +0700

    feat: update footer

--- src/features/layouts/main/footer.tsx
+++ src/features/layouts/main/footer.tsx
@@ -3,9 +3,19 @@
 export default function Footer() {
   return (
     <footer className="border-t">
-      <div className="container mx-auto px-4 py-8 text-center">
-        <p>
-          &copy; {new Date().getFullYear()} StorageFlow. All rights reserved.
+      <div className="container mx-auto px-4 py-8 flex justify-between">
+        <p className="text-sm text-muted-foreground">
+          &copy; {new Date().getFullYear()} StorageFlow. Licensed under MIT
+          License.
+        </p>
+        <p className="text-sm text-muted-foreground">
+          View the source code on{" "}
+          <a
+            href="https://github.com/wismannur/storage-flow"
+            className="text-sky-500"
+          >
+            GitHub.
+          </a>
         </p>
       </div>
     </footer>
