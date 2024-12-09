4ab32dc9e57d4ef79ce69209a97f533e02eb28b6
wismannur <wismannura@gmail.com>
2024-12-09 18:01:45 +0700

    feat: update services auth for signIn and signUp with email and password

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,5 +1,11 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
 import { createFetch } from "../constants";
 import { TSignInPayload } from "./types";
+import { firebaseClient } from "@/lib/firebase-client";
 
 export const authSignIn = async ({ email, password }: TSignInPayload) => {
   const res = await createFetch("/auth/sign-in").post({
@@ -7,4 +13,38 @@ export const authSignIn = async ({ email, password }: TSignInPayload) => {
   });
 
   return res;
+};
+
+export const authSignInWithEmail = async ({
+  email,
+  password,
+}: TSignInPayload) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      firebaseClient.auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    throw error;
+  }
+};
+
+export const authSignUpWithEmail = async (
+  email: string,
+  password: string
+): Promise<User> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      firebaseClient.auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
 };
