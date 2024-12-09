0fb34144f5b4704dc810e6e25a1f4eaa14c8e0a2
wismannur <wismannura@gmail.com>
2024-12-09 09:52:52 +0700

    feat: setup favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
 import Header from "@/features/layouts/main/header";
 import Footer from "@/features/layouts/main/footer";
 import HeroSection from "@/features/home/hero-section";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Storage Flow`,
+  description: `Effortlessly organize, track, and optimize your storage space with StorageFlow.`,
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-32x32.png",
+    },
+  ],
+  manifest: "/favicon/site.webmanifest",
+};
 
 export default function HomePage() {
   return (
